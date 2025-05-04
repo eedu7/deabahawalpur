8edf1d379036493c845c7d891b4272c812fb5b0b
eedu7 <mueedahmad067@gmail.com>
2025-05-04 17:48:00 +0500

    Created the navigation bar

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,26 @@
+interface NavigationLink {
+    title: string;
+    href: string;
+}
+
+export const navigationLinks: NavigationLink[][] = [
+    [
+        { title: "Home", href: "/home" },
+        { title: "About", href: "/about" },
+        { title: "Exam Emergency", href: "/exam-emergency" },
+        { title: "Notification", href: "/notification" },
+        { title: "Scouts", href: "/scouts" },
+        { title: "Events", href: "/events" },
+        { title: "Contact", href: "/contact" },
+    ],
+    [
+        { title: "Gallery", href: "/gallery" },
+        { title: "Initiatives", href: "/initiatives" },
+        { title: "Complaints", href: "/complaints" },
+        { title: "Model Schools", href: "/model-schools" },
+        { title: "Articles", href: "/articles" },
+        { title: "Sports", href: "/sports" },
+        { title: "Enrollment Campaign", href: "/enrollment-campaign" },
+        { title: "Team", href: "/team" },
+    ],
+];
